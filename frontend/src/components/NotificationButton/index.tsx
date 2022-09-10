331bfaaf283ea2ffe282576431fde338076578ce
Gabriel Valdivia <gabriel.softdeV@gmail.com>
2022-09-10 12:33:00 -0300

    NotificationButton Component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,11 @@
+import icon from '../../assets/img/notification-icon-50.png'
+import './styles.css'
+
+const NotificationButton = () => (
+    <div className="dsmeta-red-btn">
+        <img src={icon} alt="Notificar" />
+    </div>
+)
+
+
+export default NotificationButton
