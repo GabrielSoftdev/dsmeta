b56e5938b3956c3560404c80795c615820b49d52
Gabriel Valdivia <gabriel.softdeV@gmail.com>
2022-09-10 13:23:09 -0300

    Header and SalesCard components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,91 +1,15 @@
-import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 const App = () =>  (
   <>
-  <header>
-        <div className="dsmeta-logo-container">
-            <img src="logo.svg" alt="DSMeta" />
-            <h1>DSMeta</h1>
-            <p>
-              Desenvolvido por
-              <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
-            </p>
-        </div>
-    </header>
+    {/* HEADER */}
+    <Header/>
 
     <main>
       <section id="sales">
         <div className="dsmeta-container">
-          <div className="dsmeta-card">
-            <h2 className="dsmeta-sales-title">Vendas</h2>
-            <div>
-              <div className="dsmeta-form-control-container">
-                <input className="dsmeta-form-control" type="text"/>
-              </div>
-              <div className="dsmeta-form-control-container">
-                <input className="dsmeta-form-control" type="text"/>
-              </div>
-            </div>
-
-            <div>
-              <table className="dsmeta-sales-table">
-                <thead>
-                  <tr>
-                    <th className="show992">ID</th>
-                    <th className="show576">Data</th>
-                    <th>Vendedor</th>
-                    <th className="show992">Visitas</th>
-                    <th className="show992">Vendas</th>
-                    <th>Total</th>
-                    <th>Notificar</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td className="show992">#341</td>
-                    <td className="show576">08/07/2022</td>
-                    <td>Anakin</td>
-                    <td className="show992">15</td>
-                    <td className="show992">11</td>
-                    <td>R$ 55300.00</td>
-                    <td>
-                      <div className="dsmeta-red-btn-container">
-                        <NotificationButton/>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="show992">#341</td>
-                    <td className="show576">08/07/2022</td>
-                    <td>Anakin</td>
-                    <td className="show992">15</td>
-                    <td className="show992">11</td>
-                    <td>R$ 55300.00</td>
-                    <td>
-                      <div className="dsmeta-red-btn-container">
-                        <NotificationButton/>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="show992">#341</td>
-                    <td className="show576">08/07/2022</td>
-                    <td>Anakin</td>
-                    <td className="show992">15</td>
-                    <td className="show992">11</td>
-                    <td>R$ 55300.00</td>
-                    <td>
-                      <div className="dsmeta-red-btn-container">
-                         <NotificationButton/>
-                      </div>
-                    </td>
-                  </tr>
-                </tbody>
-
-              </table>
-            </div>
-
-          </div>
+         <SalesCard/>
         </div>
       </section>
     </main>
